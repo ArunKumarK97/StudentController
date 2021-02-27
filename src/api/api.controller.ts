e89d477e371dc55df7d1594be891bb3eebd2f538
ArunKumarK <arunkumarpg475@gmail.com>
2021-02-27 20:48:21 +0530

    Added Comments

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -4,9 +4,11 @@ import { ROOT_URL } from '../config';
 @Controller('api')
 export class ApiController {
     constructor(private readonly http: HttpService) { }
+   //Get StudentList
     @Get('/studentList')
     async getStudentList() {
-        let student = await this.http.get(ROOT_URL+'/studentList').toPromise();
+        let student = await this.http.get(ROOT_URL + '/studentList').toPromise();
+        // Order by student section
         student.data.sort((a, b) => {
             var section1 = a.section.toLowerCase(),
                 section2 = b.section.toLowerCase();
@@ -19,34 +21,38 @@ export class ApiController {
             }
             return 0;
         });
-
+        // Order by student class
         student.data.sort((a, b) => {
             return a.studentClass - b.studentClass;
         });
         return student.data;
     }
 
+    //Create Student
     @Post('/createStudent')
     async createStudent(@Body() body) {
-        const response = await this.http.post(ROOT_URL+'/studentList', body).toPromise();
+        const response = await this.http.post(ROOT_URL + '/studentList', body).toPromise();
         return response.data;
     }
 
+    //Update Student
     @Put('/studenUpdate/:id')
     async studenUpdate(@Body() body, @Param() params) {
-        const response = await this.http.put(ROOT_URL+'/studentList/' + params.id, body).toPromise();
+        const response = await this.http.put(ROOT_URL + '/studentList/' + params.id, body).toPromise();
         return response.data;
     }
 
+    //Delete Student
     @Delete('/studentDelete/:id')
     async studentDelete(@Param() params) {
-        const response = await this.http.delete(ROOT_URL+'/studentList/' + params.id).toPromise();
+        const response = await this.http.delete(ROOT_URL + '/studentList/' + params.id).toPromise();
         return response.data;
     }
 
+    //Get Student PhoneNumberList
     @Get('/getPhoneNumbers')
     async getPhoneNumbers() {
-        const students = await this.http.get(ROOT_URL+'/studentList').toPromise();
+        const students = await this.http.get(ROOT_URL + '/studentList').toPromise();
         let phoneNumbers = students.data.map(student => student.phone);
         return phoneNumbers;
     }
--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -4,6 +4,7 @@ import { ROOT_URL } from '../config';
 @Controller('login')
 export class LoginController {
     constructor(private readonly http: HttpService) {}
+    //To get user for login
     @Get()
     async getLogin() {
         const response = await this.http.get(ROOT_URL+'/login').toPromise();
